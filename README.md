067d0683084141bd926d33c0e254aab8cf351ffb
Kenji Tanaka <kenjitn@amazon.com>
2022-01-10 20:20:41 -0500

    Adding script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # renamer
-simple renamer in python for files
+simple renamer in python for files.
+
+Renames all of the files in a folder and subfolders.
+Designed to anonymize file names in reaction image folders.
+
+Usage: python renamer.py /path/to/folder
---
+++ renamer.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import random
+import string
+
+"""
+Renames all the files in a folder and subfolders to random names.
+"""
+if (len(sys.argv) is not 2):
+    raise ValueError('Usage: renamer.py /path/to/folder')
+
+dir = sys.argv[1]
+letters = string.ascii_lowercase
+
+for subdir, dirs, files in os.walk(dir):
+    for filename in files:
+        filepath = subdir + os.sep + filename
+
+        if filepath.endswith('.jpg') or filepath.endswith('.png') or filepath.endswith('.jpeg'):
+            ext = os.path.splitext(filename)[-1]
+            newname = ''.join(random.choice(letters) for i in range(32))
+            newpath = subdir + os.sep + newname + ext
+
+            os.rename(filepath, newpath)
