d916df19341e96326637198920ecc40c42b74692
Kenji Tanaka <kenjitn@amazon.com>
2022-01-10 20:29:21 -0500

    fixed formatting

--- renamer.py
+++ renamer.py
@@ -16,12 +16,7 @@ for subdir, dirs, files in os.walk(dir):
     for filename in files:
         filepath = subdir + os.sep + filename
 
-        if filepath.endswith('.jpg')
-            or filepath.endswith('.png')
-            or filepath.endswith('.jpeg')
-            or filepath.endswith('.gif')
-            or filepath.endswith('.jfif')
-            or filepath.endsiwth('.tiff'):
+        if filepath.endswith('.jpg') or filepath.endswith('.png') or filepath.endswith('.jpeg') or filepath.endswith('.gif') or filepath.endswith('.jfif') or filepath.endswith('.tiff'):
             ext = os.path.splitext(filename)[-1]
             newname = ''.join(random.choice(letters) for i in range(32))
             newpath = subdir + os.sep + newname + ext
